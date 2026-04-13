9c8226a13c5e8b0ec5e99d49dd7e2dfd0331c0fb
joellarramendia <joellarramendia@gmail.com>
2026-04-13 20:27:30 -0300

    feat: add category gallery component to the sidebar

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,8 @@
-import { PrismaClient } from "@/src/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import { categories } from "./data/categories";
 import { products } from "./data/products";
+import {prisma} from "@/src/generated/prisma/lib/prisma"
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-    adapter,
-});
 
 async function main() {
     try {
