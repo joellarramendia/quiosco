d3ab977e3f757af23c4acdcd77c70d6b23bddc33
joellarramendia <joellarramendia@gmail.com>
2026-04-13 21:07:54 -0300

    feat: add product cards to the main dashboard

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from 'next/navigation'
 import { Category } from "@/src/generated/prisma/client"
 
 type CategoryIconProps = {
@@ -7,8 +9,10 @@ type CategoryIconProps = {
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+    const params = useParams<{category: string}>()
+    
     return (
-        <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+        <div className={`${category.slug === params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
             <div className="relative size-16">
                 <Image
                     src={`/icon_${category.slug}.svg`}
@@ -16,12 +20,12 @@ export default function CategoryIcon({ category }: CategoryIconProps) {
                     fill
                 />
             </div>
-            <Link 
-            className="text-lg font-bold"
-            href={`/order/${category.slug}`}
+            <Link
+                className="text-lg font-bold"
+                href={`/order/${category.slug}`}
             >
                 {category.name}
-                </Link>
+            </Link>
 
         </div>
     )
