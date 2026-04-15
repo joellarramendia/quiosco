6f2149278e3ead5f9fb03d3092b4bbf321d493ad
joellarramendia <joellarramendia@gmail.com>
2026-04-15 19:58:04 -0300

    feat: integrate SWR for real-time data fetching

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,32 +1,29 @@
+'use client'
+
+import useSWR from 'swr'
 import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
-import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from '@/src/types';
 
-async function getPendigOrders() {
-  const orders = await prisma.order.findMany({
-    where: {
-      status: false
-    },
-    include: {
-      orderProducts: {
-        include: {
-          product: true
-        }
-      }
-    }
+
+export default function OrdersPage() {
+
+  const url = '/admin/orders/api'
+  const fetcher = () => fetch(url).then(res => res.json()).then(data => data)
+  const { data, error, isLoading } = useSWR<OrderWithProducts[]>(url, fetcher, {
+    refreshInterval: 60000,
+    revalidateOnFocus: false
   })
-  return orders
-}
 
-export default async function OrdersPage() {
-  const orders = await getPendigOrders()
-  console.log(orders)
-  return (
+  if(isLoading) return 'Cargando...'
+
+  if (data) return (
     <>
       <Heading>Administrar Ordenes</Heading>
-      {orders.length ? (
+
+      {data.length ? (
         <div className=" grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
-          {orders.map(order => (
+          {data.map(order => (
             <OrderCard
               key={order.id}
               order={order}
